ef0093bd6a52a4488757fc20645fe7b6160a35c1
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-27 13:17:23 +0700

    Add self okurigana splitter

--- main.py
+++ main.py
@@ -11,66 +11,33 @@ def is_kanji(ch):
 def is_hiragana(ch):
     return 'HIRAGANA' in unicodedata.name(ch)
 
-def split_okurigana_reverse(text, hiragana):
-    """
-      tested:
-        お茶(おちゃ)
-        ご無沙汰(ごぶさた)
-        お子(こ)さん
-    """
-    yield (text[0],)
-    yield from split_okurigana(text[1:], hiragana[1:])
-
-
-def split_okurigana(text, hiragana):
-    """ 送り仮名 processing
-      tested:
-         * 出会(であ)う
-         * 明(あか)るい
-         * 駆(か)け抜(ぬ)け
-    """
-    if is_hiragana(text[0]):
-        yield from split_okurigana_reverse(text, hiragana)
-    if all(is_kanji(_) for _ in text):
-        yield text, hiragana
-        return
-    text = list(text)
-    ret = (text[0], [hiragana[0]])
-    for hira in hiragana[1:]:
-        for char in text:
-            if hira == char:
-                text.pop(0)
-                if ret[0]:
-                    if is_kanji(ret[0]):
-                        yield ret[0], ''.join(ret[1][:-1])
-                        yield (ret[1][-1],)
-                    else:
-                        yield (ret[0],)
-                else:
-                    yield (hira,)
-                ret = ('', [])
-                if text and text[0] == hira:
-                    text.pop(0)
-                break
-            else:
-                if is_kanji(char):
-                    if ret[1] and hira == ret[1][-1]:
-                        text.pop(0)
-                        yield ret[0], ''.join(ret[1][:-1])
-                        yield char, hira
-                        ret = ('', [])
-                        text.pop(0)
-                    else:
-                        ret = (char, ret[1]+[hira])
-                else:
-                    # char is also hiragana
-                    if hira != char:
-                        break
-                    else:
-                        break
-
-
-
+def okurigana_process(text, hiragana):
+    back = []
+    front = []
+    text_pointer = [0, len(text) - 1]
+    kana_pointer = [0, len(hiragana) - 1]
+    while text_pointer[0] <= text_pointer[1]:
+        if not is_kanji(text[text_pointer[0]]):
+            front.append((text[text_pointer[0]],))
+            text_pointer[0] += 1
+            kana_pointer[0] += 1
+        else:
+            break
+    while text_pointer[0] <= text_pointer[1]:
+        if not is_kanji(text[text_pointer[1]]):
+            back.append((text[text_pointer[1]],))
+            text_pointer[1] -= 1
+            kana_pointer[1] -= 1
+        else:
+            break
+    if text_pointer[0] <= text_pointer[1]:
+        # Should handle the parts of a certain kanji, with special method
+        if(text_pointer[1] - text_pointer[0] == kana_pointer[1] - kana_pointer[0]):
+            front.extend([(text[i], hiragana[i]) for i in range(text_pointer[0], text_pointer[1] + 1)])
+        else:
+            front.append((text[text_pointer[0]:text_pointer[1] + 1], hiragana[kana_pointer[0]:kana_pointer[1] + 1]))
+    front.extend(back[::-1])
+    return front
 
 def split_furigana(text):
     """ MeCab has a problem if used inside a generator ( use yield instead of return  )
@@ -89,23 +56,22 @@ def split_furigana(text):
 
     while node is not None:
         origin = node.surface # もとの単語を代入
-        if not origin:
+        if not origin or origin == "":
             node = node.next
             continue
-
+        print(origin, node.feature)
         # originが空のとき、漢字以外の時はふりがなを振る必要がないのでそのまま出力する
-        if origin != "" and any(is_kanji(_) for _ in origin):
-            #sometimes MeCab can't give kanji reading, and make node-feature have less than 7 when splitted.
+        if any(is_kanji(_) for _ in origin):
+            #sometimes MeCab can't give kanji reading, and make node-feature have less than 9 when splitted.
             #bypass it and give kanji as isto avoid IndexError
-            print(origin, node.feature)
+            # print(origin, node.feature)
             if len(node.feature.split(",")) > 9:
                 kana = node.feature.split(",")[9] # 読み仮名を代入
             else:
                 kana = node.surface
             hiragana = jaconv.kata2hira(kana)
             # print(origin, kana, hiragana)
-            for pair in split_okurigana(origin, hiragana):
-                ret += [pair]
+            ret += okurigana_process(origin, hiragana)
         else:
             if origin:
                 ret += [(origin,)]
@@ -114,7 +80,9 @@ def split_furigana(text):
 
 
 def print_html(text, file=None):
-    for pair in split_furigana(text):
+    furigana = split_furigana(text)
+    print(furigana)
+    for pair in furigana:
         if len(pair)==2:
             kanji,hira = pair
             print("<ruby><rb>{0}</rb><rt>{1}</rt></ruby>".
@@ -125,8 +93,8 @@ def print_html(text, file=None):
 
 
 def print_plaintext(text, file=None):
-    for pair in split_furigana(text):
-        if len(pair)==2:
+    for pair in split_furigana(text)[0]:
+        if len(pair) == 2:
             kanji,hira = pair
             print("%s(%s)" % (kanji,hira), end='', file=file)
         else:
@@ -135,7 +103,15 @@ def print_plaintext(text, file=None):
 
 
 def main():
-    text = '澱んだ街角で僕らは出会った'
+    text = '澱んだ街角で,僕らは出会った'
+    # text = '活版印刷の流れを汲む出版作業では'
+    # text = 'お茶にお煎餅、よく合いますね'
+    # text = '野ブタ。をプロデュース'
+    # text = '本当に'
+    # text = '平気'
+    # text = '平然'
+    # text = '格好いい'
+    # text = '庭には２羽鶏がいる'
     with open('tmp.html', 'w') as buffer:
         print_html(text,buffer)
 
